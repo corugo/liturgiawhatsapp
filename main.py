30b331a9be3466a86ef8a442b34b4062181a2746
corugo <victor_hugo_ventura@hotmail.com>
2022-01-08 08:25:50 -0400

    Corrigidos alguns bugs

--- main.py
+++ main.py
@@ -11,12 +11,14 @@ subitem = Nome item
 url = URL
 '''
 import os
+import pprint
+from PyQt5.QtWidgets import QApplication, QLabel
 
 semana = {1:'Domingo', 2:'Segunda-Feira', 3:'Terça-Feira', 4:'Quarta-Feira', 5:'Quinta-Feira', 6:'Sexta-Feira', 7:'Sábado'}
 itens = {}
 geral = {}
 tempitens = {}
-item = "a"
+item = ""
 
 def trataArquivo(diretorio):
     splitado = diretorio.split('\\')
@@ -35,38 +37,45 @@ with open(os.getenv('APPDATA') + "\LouvorJA\liturgia.ja") as input_file:
                 nomeitem = line
                 nomeitem = nomeitem.replace("[","")
                 nomeitem = nomeitem.replace("]","")
-                item = nomeitem.rstrip('\n')
+                item = nomeitem.rstrip('\n')    #apagar \n
             elif item[0] == "G":
-                temp = line.split('=')
-                geral[temp[0]] = temp[1].split(';')
+                if line[0] != "A":
+                    temp = line.split('=')
+                    temp[1] = temp[1].rstrip(';\n')
+                    geral[temp[0]] = temp[1].split(';')
             else:
                 temp = line.split('=')
                 tempitens[temp[0]] = temp[1].rstrip('\n')
     itens[item] = tempitens
-print(itens)
 
-for i in range(1,8):
-    print("\nDia da semana: "+semana[i])
-    try:
-        for x in geral[str(i)]:
-            #print(itens[x]["item"])
-            if itens[x]["tipo"] == "categoria":
-                print("\n*" + itens[x]["item"].upper() + "*")
-            else:
-                print("- " + itens[x]["item"])
-                if itens[x]["tipo"] == "musica":        #Música
-                    if itens[x]["subitem"] == "Clique para escolher a música":
-                        print("  *_Música não selecionada_*")
-                    else:
-                        print("  _"+ itens[x]["subitem"]+ "_")
-                elif itens[x]["tipo"] == "anotacao":    #Anotação
-                    if itens[x]["subitem"] != "":
-                        print("  _"+ itens[x]["subitem"]+ "_")
-                elif itens[x]["tipo"] == "arquivo":     #Arquivo
-                    if itens[x]["dir"] != "":
-                        print("  _Arquivo "+ trataArquivo(itens[x]["dir"]) + "_")
-                    else:
-                        print("  *_Arquivo não selecionado_*")
-            
-    except:
-        pass
+pprint.pprint(itens)
+pprint.pprint(geral)
+
+for i in geral:
+    print("\nDia da semana: "+semana[int(i)])
+    for x in geral[str(i)]:
+        #print(itens[x]["item"])
+        if itens[x]["tipo"] == "categoria":         #Categoria
+            print("\n*" + itens[x]["item"].upper() + "*\n")
+        else:
+            print("- " + itens[x]["item"])
+            if itens[x]["tipo"] == "musica":        #Música
+                if itens[x]["subitem"] == "Clique para escolher a música":
+                    print("  *_Música não selecionada_*")
+                else:
+                    print("  _"+ itens[x]["subitem"]+ "_")
+            elif itens[x]["tipo"] == "anotacao":    #Anotação
+                if itens[x]["subitem"] != "":
+                    print("  _"+ itens[x]["subitem"]+ "_")
+            elif itens[x]["tipo"] == "arquivo":     #Arquivo
+                if itens[x]["dir"] != "":
+                    print("  _Arquivo "+ trataArquivo(itens[x]["dir"]) + "_")
+                else:
+                    print("  *_Arquivo não selecionado_*")
+
+'''
+app = QApplication([])
+label = QLabel('Hello World!')
+label.show()
+app.exec()
+'''
