be7ab638f60758fe4def132bf36d9e222096ee05
corugo <victor_hugo_ventura@hotmail.com>
2022-03-26 11:19:42 -0400

    Atualização no layout e adição da função lista

--- main.py
+++ main.py
@@ -23,6 +23,8 @@ geral = {}
 tempitens = {}
 item = ""
 tamanhofonte = 16
+entrelinhas = round(tamanhofonte * 0.2) + tamanhofonte
+espacamento = 2
 
 def tcolor(cor):
     if cor[0] == "$":
@@ -71,22 +73,23 @@ for i in geral:
         continue
     
     for x in geral[str(i)]:
-        #print(x)
-        #print(itens[x])
-        if itens[x]["tipo"] == "itensagendados":
-            tamanho += tamanhofonte
-        elif itens[x]["tipo"] == "anotacao" and itens[x]["subitem"] == "":
-            tamanho += tamanhofonte
+        if itens[x]["tipo"] == "itensagendados" or itens[x]["item"].lower() == "lista":
+            tamanho += entrelinhas
+        elif itens[x]["tipo"] == "anotacao" and (itens[x]["subitem"] == "" or itens[x]["subitem"] == "  "):
+            tamanho += entrelinhas
         else:
-            tamanho += tamanhofonte * 2
-    tamanho += tamanhofonte
+            tamanho += entrelinhas * 2
+        tamanho += espacamento
+    tamanho += entrelinhas
     
-    img = Image.new('RGB', (600, tamanho), color = (0,0,0))
+    img = Image.new('RGB', (600, tamanho), color = (202,187,170))
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype("cour.ttf", tamanhofonte)
-    font2 = ImageFont.truetype("courbd.ttf", tamanhofonte)
-    font3 = ImageFont.truetype("courbi.ttf", tamanhofonte)
-    font4 = ImageFont.truetype("couri.ttf", tamanhofonte)
+    font = ImageFont.truetype("arial.ttf", tamanhofonte)
+    font2 = ImageFont.truetype("arialbd.ttf", tamanhofonte)
+    font3 = ImageFont.truetype("arialbi.ttf", tamanhofonte)
+    font4 = ImageFont.truetype("ariali.ttf", tamanhofonte)
+    corFonteItens = (0,0,0)
+    corFonteCategorias = (255,255,255)
     # draw.text((x, y),"Sample Text",(r,g,b))
     #draw.text((0, 0),"Sample Text",(255,255,255),font=font)
     
@@ -95,40 +98,50 @@ for i in geral:
     
     draw.text((230, 0),semana[int(i)],(255,255,255),font=font2)
     
-    #print(offset+tamanhofonte)
-    #print(tamanhofonte)
     for x in geral[str(i)]:
-        print(itens[x]["item"])
-        print(tcolor(itens[x]["cor"]))
         if itens[x]["tipo"] == "categoria":         #Categoria
-            offset += tamanhofonte * 2
-            draw.rectangle(((0, offset), (400, offset +tamanhofonte)), fill=tcolor(itens[x]["cor"]))
-            draw.text((16, offset),itens[x]["item"].upper(),(255,255,255),font=font2)
+            offset += entrelinhas * 2
+            draw.rectangle(((0, offset), (600, offset +entrelinhas-1)), fill=tcolor(itens[x]["cor"]))
+            draw.text((16, offset),itens[x]["item"].upper(),corFonteCategorias,font=font2)
+            draw.rectangle(((0, offset +entrelinhas-2), (600, offset +entrelinhas-1)), fill=(187,168,147)) #Linha inferior
+        
+        elif itens[x]["tipo"] == "musica":        #Música
+            if itens[x]["item"].lower() != "lista":
+                offset += entrelinhas
+                draw.rectangle(((4, offset + (entrelinhas * 0.3)), (12, offset + (entrelinhas * 0.7))), fill=tcolor(itens[x]["cor"]))
+                draw.text((16, offset),itens[x]["item"],corFonteItens,font=font)
+            if itens[x]["subitem"] == "Clique para escolher a música":
+                offset += entrelinhas
+                draw.text((16, offset),"  Música não selecionada",(219,49,49),font=font3)
+
+            else:
+                offset += entrelinhas
+                draw.text((16, offset),"  " + itens[x]["subitem"],corFonteItens,font=font4)
+            draw.rectangle(((0, offset +entrelinhas), (600, offset +entrelinhas + espacamento -1)), fill=(187,168,147)) #Linha inferior
+
         else:
-            offset += tamanhofonte
-            draw.rectangle(((0, offset), (10, offset +tamanhofonte)), fill=tcolor(itens[x]["cor"]))
-            draw.text((16, offset),itens[x]["item"],(255,255,255),font=font)
-            if itens[x]["tipo"] == "musica":        #Música
-                if itens[x]["subitem"] == "Clique para escolher a música":
-                    offset += tamanhofonte
-                    draw.text((16, offset),"  Música não selecionada",(255,5,5),font=font3)
-                else:
-                    offset += tamanhofonte
-                    draw.text((16, offset),"  " + itens[x]["subitem"],(255,255,255),font=font4)
-            elif itens[x]["tipo"] == "anotacao":    #Anotação
-                if itens[x]["subitem"] != "":
-                    offset += tamanhofonte
-                    draw.text((16, offset),"  " + itens[x]["subitem"],(255,255,255),font=font4)
+            offset += entrelinhas
+            draw.rectangle(((4, offset + (entrelinhas * 0.3)), (12, offset + (entrelinhas * 0.7))), fill=tcolor(itens[x]["cor"]))
+            draw.text((16, offset),itens[x]["item"],corFonteItens,font=font)
+
+            if itens[x]["tipo"] == "anotacao":    #Anotação
+                if itens[x]["subitem"] != "" and itens[x]["subitem"] != "  ":
+                    offset += entrelinhas
+                    draw.text((16, offset),"  " + itens[x]["subitem"],corFonteItens,font=font4)
+
             elif itens[x]["tipo"] == "arquivo":     #Arquivo
                 if itens[x]["dir"] != "":
-                    offset += tamanhofonte
-                    draw.text((16, offset),"  Arquivo " + trataArquivo(itens[x]["dir"]),(255,255,255),font=font4)
+                    offset += entrelinhas
+                    draw.text((16, offset),"  Arquivo " + trataArquivo(itens[x]["dir"]),corFonteItens,font=font4)
+                
                 else:
-                    offset += tamanhofonte
-                    draw.text((16, offset),"  Arquivo não selecionado",(255,5,5),font=font3)
-                    
-    img.save(str(i) +'.jpg')
-    print(itens)
+                    offset += entrelinhas
+                    draw.text((16, offset),"  Arquivo não selecionado",(219,49,49),font=font3)
+            draw.rectangle(((0, offset +entrelinhas), (600, offset +entrelinhas + espacamento -1)), fill=(187,168,147)) #Linha inferior
+        offset += espacamento
+    draw.rectangle(((0, 0), (599, tamanho-1)), width=2, outline=(0,0,0)) #contorno
+    img.save(str(i) +'.png')
+    #print(itens)
     
 
 
