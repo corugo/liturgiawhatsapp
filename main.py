71c88b73d2a0a34b5f5a5faaaabc5c1f2fbe8d60
corugo <victor_hugo_ventura@hotmail.com>
2022-06-25 09:37:47 -0400

    Remoção de código não utilizado e melhoria de código

--- main.py
+++ main.py
@@ -14,8 +14,8 @@ import os
 import pprint
 from PIL import Image
 from PIL import ImageFont
-from PIL import ImageDraw 
-#from PyQt5.QtWidgets import QApplication, QLabel
+from PIL import ImageDraw
+
 cl = {"clBlack":(0,0,0),"clMaroon":(128, 0, 0),"clGreen":(0, 128, 0),"clOlive":(128,128,0),"clNavy":(0,0,128),"clPurple":(128,0,128),"clTeal":(128,128,0),"clGray":(128,128,128),"clSilver":(192,192,192),"clRed":(255, 0, 0),"clLime":(0,255,0),"clYellow":(255,255,0),"clBlue":(0,0,255),"clFuchsia":(255,0,255),"clAqua":(0,255,255),"clLtGray":(192,192,192),"clDkGray":(128,128,128),"clWhite":(255,255,255),"clCream":(255, 251, 240),"clMedGray":(160, 160, 164),"clMoneyGreen":(192, 220, 192),"clSkyBlue":(166, 202, 240)}
 semana = {1:'Domingo', 2:'Segunda-Feira', 3:'Terça-Feira', 4:'Quarta-Feira', 5:'Quinta-Feira', 6:'Sexta-Feira', 7:'Sábado'}
 itens = {}
@@ -40,9 +40,6 @@ def trataArquivo(diretorio):
     exten = splitado.pop()
     tratado = exten#[:30]
     return(tratado)
-
-#dicionario = {"teste":{"testedici":"testecerto"}}
-#print(dicionario["teste"]["testedici"])
 
 with open(os.getenv('APPDATA') + "\LouvorJA\liturgia.ja") as input_file:
     for line in input_file:
@@ -75,7 +72,7 @@ for i in geral:
     for x in geral[str(i)]:
         if itens[x]["tipo"] == "itensagendados" or itens[x]["item"].lower() == "lista":
             tamanho += entrelinhas
-        elif itens[x]["tipo"] == "anotacao" and (itens[x]["subitem"] == "" or itens[x]["subitem"] == "  "):
+        elif itens[x]["tipo"] == "anotacao" and itens[x]["subitem"].replace(" ","") == "":
             tamanho += entrelinhas
         else:
             tamanho += entrelinhas * 2
@@ -90,10 +87,6 @@ for i in geral:
     font4 = ImageFont.truetype("ariali.ttf", tamanhofonte)
     corFonteItens = (0,0,0)
     corFonteCategorias = (255,255,255)
-    # draw.text((x, y),"Sample Text",(r,g,b))
-    #draw.text((0, 0),"Sample Text",(255,255,255),font=font)
-    
-    #tcolor(itens[x]["cor"])
     
     
     draw.text((230, 0),semana[int(i)],(255,255,255),font=font2)
@@ -110,7 +103,7 @@ for i in geral:
                 offset += entrelinhas
                 draw.rectangle(((4, offset + (entrelinhas * 0.3)), (12, offset + (entrelinhas * 0.7))), fill=tcolor(itens[x]["cor"]))
                 draw.text((16, offset),itens[x]["item"],corFonteItens,font=font)
-            if itens[x]["subitem"] == "Clique para escolher a música":
+            if itens[x]["subitem"].find("Clique para escolher a música") == 0:
                 offset += entrelinhas
                 draw.text((16, offset),"  Música não selecionada",(219,49,49),font=font3)
 
@@ -125,7 +118,7 @@ for i in geral:
             draw.text((16, offset),itens[x]["item"],corFonteItens,font=font)
 
             if itens[x]["tipo"] == "anotacao":    #Anotação
-                if itens[x]["subitem"] != "" and itens[x]["subitem"] != "  ":
+                if itens[x]["subitem"].replace(" ","") != "":
                     offset += entrelinhas
                     draw.text((16, offset),"  " + itens[x]["subitem"],corFonteItens,font=font4)
 
@@ -141,43 +134,3 @@ for i in geral:
         offset += espacamento
     draw.rectangle(((0, 0), (599, tamanho-1)), width=2, outline=(0,0,0)) #contorno
     img.save(str(i) +'.png')
-    #print(itens)
-    
-
-
-
-'''
-pprint.pprint(itens)
-pprint.pprint(geral)
-
-for i in geral:
-    print("\nDia da semana: "+semana[int(i)])
-    if geral[str(i)] == ['']:   #Se for um dia vazio
-        print("Dia vazio")
-        continue
-    for x in geral[str(i)]:
-        #print(itens[x]["item"])
-        if itens[x]["tipo"] == "categoria":         #Categoria
-            print("\n*" + itens[x]["item"].upper() + "*\n")
-        else:
-            print("- " + itens[x]["item"])
-            if itens[x]["tipo"] == "musica":        #Música
-                if itens[x]["subitem"] == "Clique para escolher a música":
-                    print("  *_Música não selecionada_*")
-                else:
-                    print("  _"+ itens[x]["subitem"]+ "_")
-            elif itens[x]["tipo"] == "anotacao":    #Anotação
-                if itens[x]["subitem"] != "":
-                    print("  _"+ itens[x]["subitem"]+ "_")
-            elif itens[x]["tipo"] == "arquivo":     #Arquivo
-                if itens[x]["dir"] != "":
-                    print("  _Arquivo "+ trataArquivo(itens[x]["dir"]) + "_")
-                else:
-                    print("  *_Arquivo não selecionado_*")
-'''
-'''
-app = QApplication([])
-label = QLabel('Hello World!')
-label.show()
-app.exec()
-'''
